14aa1eda0fca367e82f5ac5c5a51ea9cfb7dff65
11qweASD <mt6323345@gmail.com>
2026-04-25 15:24:31 +0800

    Site updated: 2026-04-25 15:24:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["k/My-Newst/","k/My-st/","k/My-Nest/","k/hello-world/"];function toRandomPost(){
+var posts=["k/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
